a988cddaf7f2597600d341aa27c21def13306f5a
Rethy729 <hjn0729@gmail.com>
2025-03-10 13:03:46 +0900

    Find an Eulerian Graph in a Graph

--- BA3/BA3F/BA3F.py
+++ BA3/BA3F/BA3F.py
@@ -13,32 +13,29 @@ for edge in edge_data:
 def find_cycle(start_node, graph):
 
     cycle = []
-    arr_node = graph[start_node].pop()
+    arr_node = graph[start_node].pop()   #pop in dictionary, the first "walk"
 
-    while arr_node != start_node:
-        cycle.append(arr_node)          
-        arr_node = graph[arr_node].pop()
-    cycle.append(arr_node)
+    while arr_node != start_node:         #while the arrival node != start node, which means it forms a loop
+        cycle.append(arr_node)            #append the arrival node
+        arr_node = graph[arr_node].pop()  #next "walk"
+    cycle.append(arr_node)                #append the last arrival node, which is same as start_node
 
-    for key in list(graph):
+    for key in list(graph):               #delete the empty key
         if graph[key] == []:
             del graph[key]
     
-    return cycle
-
+    return cycle                          #eventually, the cycle is like this (start_node = 1 -> [2, 4, 6, 8, 3, 1] (1 is not included in the beginning)
 
 def EulerianCycle(start_node, graph):
-    cycle = [start_node] + find_cycle(start_node, graph)
-
+    cycle = [start_node] + find_cycle(start_node, graph)       #start from an initial loop, which includes the start node at the beginning
     break_bool = True
     while break_bool:
         break_bool = False
         for i, node in enumerate(cycle):
-            if node in graph:
-                break_bool = True
+            if node in graph:                                  #finding an unused edge starting from some node in cycle
+                break_bool = True                              #if no unused edge, break_bool does not change to True, while loop eventually breaks
                 cycle = cycle[:i+1] + find_cycle(node, graph) + cycle[i+1:]
                 break
-
     return cycle
 
 answer = EulerianCycle(0, graph)
